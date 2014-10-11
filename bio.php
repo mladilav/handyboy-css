bd440af85aa41d4299b85a1b4f89ad2ed3c1da39
pl2 <pl2@mail.ru>
2014-10-11 13:30:16 +0300

    dada

--- bio.php
+++ bio.php
@@ -8,7 +8,6 @@
     <script src="//code.jquery.com/jquery-1.10.2.js"></script>
     <script  src="//code.jquery.com/ui/1.11.1/jquery-ui.js"></script>
     <script type="text/javascript" src="/js/scripts.js"></script>
-
     <link rel='stylesheet' href="/css/vendor/jquery-ui.css"/>
     <link rel="stylesheet" href="/fonts/font.css"/>
     <link rel="stylesheet" href="/css/style.css"/>
@@ -63,7 +62,7 @@
                                     </p>
                                     <label class="small-label">Ethnicity</label>
                                     <p>
-                                        <select class="select">
+                                        <select class="select-bio">
                                             <option>What ethnicity for you identify...</option>
                                             <option>2</option>
                                             <option>3</option>
@@ -72,7 +71,7 @@
 
                                     <label class="small-label">Hair color</label>
                                     <p>
-                                        <select class="select">
+                                        <select class="select-bio">
                                             <option>What color's your mop?</option>
                                             <option>2</option>
                                             <option>3</option>
@@ -81,7 +80,7 @@
 
                                     <label class="small-label">Eye color</label>
                                     <p>
-                                        <select class="select">
+                                        <select class="select-bio">
                                             <option>Lapis Lazuli or Viridian?</option>
                                             <option>2</option>
                                             <option>3</option>
@@ -90,7 +89,7 @@
 
                                     <label class="small-label">Body type</label>
                                     <p>
-                                        <select class="select">
+                                        <select class="select-bio">
                                             <option>How are you built/sculped?</option>
                                             <option>2</option>
                                             <option>3</option>
@@ -104,17 +103,143 @@
                                 </div>
                             </li>
                             <li><div class="accordion-title" id="work-schedule"><b>+</b> <span> Work Schedule</span></div>
-                                <div class="inner-body"> Two Two Two Two</div>
+                                <div class="inner-body">
+                                    <p class="small-title-light">Set your work schedule</p>
+                                    <p class="bio-text">
+                                        Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                                        Aenean commodo ligula eget dolor. Aenean massa. Cum sociis
+                                        natoque penatibus et magnis dis parturient montes, nascetur
+                                        ridiculus mus. Donec quam felis, ultricies nec, pellentesque
+                                        eu, pretium quis, sem. Nulla consequat massa quis enim. Donec
+                                        pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
+                                        In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.
+
+                                    </p>
+
+                                    <div class="calendar">
+                                        <ul class="calendar-week">
+                                            <li><a href="">SUN</a></li>
+                                            <li class="active"><a href="">MON</a></li>
+                                            <li><a href="">TUE</a></li>
+                                            <li><a href="">WED</a></li>
+                                            <li><a href="">THUR</a></li>
+                                            <li><a href="">FRI</a></li>
+                                            <li><a href="">SAT</a></li>
+                                        </ul>
+                                        <div class="calendar-days">
+                                            <div class="calendar-days-item">8A</div>
+                                            <div class="calendar-days-item">8:30A</div>
+                                            <div class="calendar-days-item">9A</div>
+                                            <div class="calendar-days-item active">9:30A</div>
+                                            <div class="calendar-days-item active">10A</div>
+                                            <div class="calendar-days-item active">10:30A</div>
+                                            <div class="calendar-days-item active">11A</div>
+                                            <div class="calendar-days-item active">11:30A</div>
+                                            <div class="calendar-days-item active">12A</div>
+                                            <div class="calendar-days-item">12:30A</div>
+                                            <div class="calendar-days-item">1P</div>
+                                            <div class="calendar-days-item">1:30P</div>
+                                            <div class="calendar-days-item">2P</div>
+                                            <div class="calendar-days-item">2:30P</div>
+                                            <div class="calendar-days-item">3P</div>
+                                            <div class="calendar-days-item">3:30P</div>
+                                            <div class="calendar-days-item">4P</div>
+                                            <div class="calendar-days-item">4:30P</div>
+                                            <div class="calendar-days-item">5P</div>
+                                            <div class="calendar-days-item">5:30P</div>
+                                            <div class="calendar-days-item">6P</div>
+                                            <div class="calendar-days-item">6:30P</div>
+                                            <div class="calendar-days-item">7P</div>
+                                            <div class="calendar-days-item">7:30P</div>
+                                            <div class="calendar-days-item">8P</div>
+                                            <div class="calendar-days-item">8:30P</div>
+                                            <div class="calendar-days-item">9P</div>
+                                            <div class="calendar-days-item">9:30P</div>
+                                            <div class="calendar-days-item">10P</div>
+                                            <div class="calendar-days-item">10:30P</div>
+                                            <div class="calendar-days-item">11P</div>
+                                            <div class="calendar-days-item">11:30P</div>
+                                            <div class="calendar-days-item">12P</div>
+                                            <div class="calendar-days-item">12:30P</div>
+                                            <div class="calendar-days-item">1A</div>
+                                            <div class="calendar-days-item">1:30A</div>
+                                            <div class="calendar-days-item">2A</div>
+                                            <div class="calendar-days-item">2:30A</div>
+                                            <div class="calendar-days-item">3A</div>
+                                            <div class="calendar-days-item">3:30A</div>
+                                            <div class="calendar-days-item">4A</div>
+                                            <div class="calendar-days-item">4:30A</div>
+                                            <div class="calendar-days-item">5A</div>
+                                            <div class="calendar-days-item">5:30A</div>
+                                            <div class="calendar-days-item">6A</div>
+                                            <div class="calendar-days-item">6:30A</div>
+                                            <div class="calendar-days-item">7A</div>
+                                            <div class="calendar-days-item">7:30A</div>
+                                        </div>
+                                    </div>
+
+                                    <p class="small-title-light">Customize your calendar</p>
+                                    <p class="bio-text">
+                                        Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                                        Aenean commodo ligula eget dolor. Aenean massa. Cum sociis
+                                        natoque penatibus et magnis dis parturient montes, nascetur
+                                        ridiculus mus. Donec quam felis, ultricies nec, pellentesque
+                                        eu, pretium quis, sem.
+
+                                    </p>
+                                </div>
                             </li>
-                            <li><div class="accordion-title" id="job-details"><b>+</b> <span> Job Details De</span></div>
-                                <div class="inner-body"> Three Three Three Three</div>
+                            <li><div class="accordion-title" id="job-details"><b>+</b> <span> Job Details</span></div>
+                                <div class="inner-body">
+                                    <div class="category-jobs">
+                                        <p class="category-jobs-item category-jobs-item-1 active"></p>
+                                        <p class="category-jobs-item category-jobs-item-2"></p>
+                                        <p class="category-jobs-item category-jobs-item-3"></p>
+                                        <p class="category-jobs-item category-jobs-item-4"></p>
+                                        <p class="category-jobs-item category-jobs-item-5"></p>
+                                        <p class="category-jobs-item category-jobs-item-6"></p>
+                                        <p class="category-jobs-item category-jobs-item-7"></p>
+                                        <p class="category-jobs-item category-jobs-item-8"></p>
+                                        <p class="category-jobs-item category-jobs-item-9"></p>
+                                        <p class="category-jobs-item category-jobs-item-10"></p>
+                                    </div>
+                                </div>
                             </li>
                         </ul>
                     </div>
 
                 </div>
                 <div class="rightPart">
-                    <img src="img/user.png">
+                    <div class="users-avatars">
+                        <div class="current-image">
+                            <img src="/img/user.png">
+                        </div>
+                        <div class="smalls-avatars">
+                            <img src="/img/user.png">
+                            <img src="/img/user.png">
+                            <img src="/img/user.png">
+                            <img src="/img/user.png">
+                            <img src="/img/user.png">
+                            <button class="add-photo-profile">
+
+                            </button>
+                        </div>
+                    </div>
+                    <div class="user-profile-complete">
+                        <div class="user-profile-complete-header">
+                            Percent complete <i class="icon-info-white"></i>
+                        </div>
+                        <p class="user-profile-complete-next-step">
+                            Next steps
+                        </p>
+                        <ul class="user-profile-complete-steps">
+                            <li>Add photo +5%</li>
+                            <li>Bio +15%</li>
+                            <li>Search profile +5%</li>
+                            <li>Schedule +20%</li>
+                            <li>Job Details +15%</li>
+                        </ul>
+                    </div>
                 </div>
 
 
@@ -137,7 +262,7 @@
 
 <script>
 
-    $( ".select" ).selectmenu({ width: 570 });
+    $( ".select-bio" ).selectmenu({ width: 650 });
 
 
 </script>
